913f7ea600d907f1eb78026bd7c0da59a05217b5
Warwick <krugazul@gmail.com>
2023-03-24 16:05:30 +0200

    Updating the block rendering functions.

--- includes/class-core.php
+++ includes/class-core.php
@@ -15,9 +15,8 @@ class Core {
     }
 
 	public function init() {
-		add_action( 'enqueue_block_editor_assets', array( $this, 'register_block_variations' ) );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'register_blocks' ) );
 		add_action( 'init', array( $this, 'register_block_type' ), 20 );
-		add_filter( 'render_block', array( $this, 'render_mega_menu_block' ), 10, 2 );
 	}
 
 	/**
@@ -25,7 +24,7 @@ class Core {
 	 *
 	 * @return void
 	 */
-	public function register_block_variations() {
+	public function register_blocks() {
 		wp_enqueue_script(
 			'lsx-mega-menu-block',
 			LSX_MEGAMENU_URL . '/build/index.js',
@@ -49,34 +48,54 @@ class Core {
 		register_block_type(
 			LSX_MEGAMENU_PATH . 'src/menu-item/',
 			array(
-				'render_callback' => 'render_block_core_navigation_link',
+				'render_callback' => array( $this, 'render_mega_menu_block_new' ),
 			)
 		);
 	}
 
 	/**
-	 * Renders our mega menu template part.
+	 * Renders the `core/navigation-link` block.
 	 *
-	 * @param string $block_content
-	 * @param array $block
-	 * @return string
+	 * @param array    $attributes The block attributes.
+	 * @param string   $content    The saved content.
+	 * @param WP_Block $block      The parsed block.
+	 *
+	 * @return string Returns the post content with the legacy widget added.
 	 */
-	public function render_mega_menu_block( $block_content = '', $block = [] ) {
-		if ( ! isset( $block['blockName'] ) || 'lsx/lsx-mega-menu' === $block['blockName'] ) {
-			return $block_content;
+	public function render_mega_menu_block_new( $attributes, $content, $block ) {
+
+		// Don't render the block's subtree if it has no label.
+		if ( empty( $attributes['menu'] ) ) {
+			return '';
 		}
 
-		$menu_name = trim( strip_tags( $block_content ) );
-		if ( '' === $menu_name ) {
-			return $block_content;
-		}
-
-		$menu = get_page_by_path( $menu_name, OBJECT, 'wp_template_part' );
+		// Check if the menu exists
+		$menu = get_page_by_path( $attributes['menu'], OBJECT, 'wp_template_part' );
 		if ( null === $menu ) {
-			return $block_content;
+			return '';
 		}
 
-		$block_content = apply_filters( 'the_content', $menu->post_content );
-		return $block_content;
+		$font_sizes      = block_core_navigation_link_build_css_font_sizes( $block->context );
+		$classes         = array_merge(
+			$font_sizes['css_classes']
+		);
+		$style_attribute = $font_sizes['inline_styles'];
+
+		$css_classes = trim( implode( ' ', $classes ) );
+		$is_active   = ! empty( $attributes['id'] ) && ( get_queried_object_id() === (int) $attributes['id'] );
+
+		$wrapper_attributes = get_block_wrapper_attributes(
+			array(
+				'class' => $css_classes . ' wp-block-navigation-item' .
+					( $is_active ? ' current-menu-item' : '' ),
+				'style' => $style_attribute,
+			)
+		);
+
+		$html  = '<li ' . $wrapper_attributes . '>' . $attributes['menu'];
+		$html .= apply_filters( 'the_content', $menu->post_content );
+		$html .= '</li>';
+
+		return $html;
 	}
 }
--- src/mega-menu/save.js
+++ src/mega-menu/save.js
@@ -5,8 +5,6 @@ import { createElement } from "@wordpress/element";
  */
 import { InnerBlocks } from '@wordpress/block-editor';
 export default function save() {
-	console.log('Menu Save');
-	console.log(createElement(InnerBlocks.Content, null));
   return createElement(InnerBlocks.Content, null);
 }
 //# sourceMappingURL=save.js.map
